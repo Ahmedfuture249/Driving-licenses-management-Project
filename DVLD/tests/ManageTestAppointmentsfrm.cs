90899af67fcde23a2f666c58509de8f2449cfb6f
Ahmed Ali <A7medwhman@gmail.com>
2026-02-17 02:30:15 +0200

    Organizing the Project

---
+++ DVLD/tests/ManageTestAppointmentsfrm.cs
@@ -0,0 +1,93 @@
+﻿using DVLDBusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD
+{
+    public partial class ManageTestAppointmentsfrm: Form
+    {
+       static int LocalDrivingLicenseApplicationID;
+        clsLDLApplication LDLApplication = clsLDLApplication.FindLocalDrivingLicenseApplication(LocalDrivingLicenseApplicationID);
+        public int TestTypeID { set; get; }
+        static clsTestAppointment appointment = clsTestAppointment.FindByLDLApp(LocalDrivingLicenseApplicationID);
+       
+        public ManageTestAppointmentsfrm(int TestTypeID, int ID)
+        {
+            InitializeComponent();
+            LocalDrivingLicenseApplicationID=ctrlDrivingLicenseApplicationInfocs1.LocalDrivingLicenseApplicationID = ID;
+            this.TestTypeID = TestTypeID;
+            
+            ctrlDrivingLicenseApplicationInfocs1._LoadApplicationInfo(LocalDrivingLicenseApplicationID);
+            switch(this.TestTypeID)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.Vision_512;
+                    lblTitle.Text = "Vision Test Appointment ";
+                    
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.Written_Test_512;
+                    lblTitle.Text = "Written Test Appointment ";
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.driving_test_512;
+                    lblTitle.Text = "Driving Test Appointment ";
+                    break;
+                default:
+                    pictureBox1.Image = Properties.Resources.Vision_512;
+                    break;
+
+            }
+            
+        }
+        
+        private void _RefreshList()
+        {
+            dgvGetAllAppointments.DataSource = clsTestAppointment.ListTestAppointments(LocalDrivingLicenseApplicationID,TestTypeID);
+        }
+       
+        private void ManageTestAppointmentsfrm_Load(object sender, EventArgs e)
+        {
+            _RefreshList();
+        }
+
+        private void btnAddNeAppointment_Click(object sender, EventArgs e)
+        {
+           //if( clsTestAppointment.IsApplicantHasAnActiveAppointmentForThisTest(LocalDrivingLicenseApplicationID, TestTypeID))
+           // {
+              
+           //     MessageBox.Show("this person already sat for this test");
+           //     return;
+           // }
+           
+            ScheduleTestfrm frm = new ScheduleTestfrm(TestTypeID, LocalDrivingLicenseApplicationID);
+            frm.ShowDialog();
+        }
+
+        private void takeTestToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(clsLDLApplication.DoesPassTestType(LocalDrivingLicenseApplicationID, TestTypeID))
+            {
+                MessageBox.Show("this peroson already Pass this test Move to the Next Test");
+                return;
+            }
+           
+            int i = (int)dgvGetAllAppointments.CurrentRow.Cells[0].Value;
+            TakeTestfrm frm = new TakeTestfrm(i);
+            frm.ShowDialog();
+            _RefreshList();
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DVLD/tests/TakeTestfrm.cs
@@ -0,0 +1,62 @@
+﻿using DVLDBusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD
+{
+    public partial class TakeTestfrm: Form
+    {
+        clsTest Test;
+        static public int TestAppointmentID;
+        clsTestAppointment TestAppointment;
+        public TakeTestfrm(int TestAppointmentid)
+        {
+            InitializeComponent();
+            TestAppointmentID = TestAppointmentid;
+            TestAppointment= clsTestAppointment.Find(TestAppointmentID);
+            ctrlTakeTest1.LoadData(TestAppointmentID);
+        }
+
+        private void TakeTestfrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Test = new clsTest();
+            if (rbyes.Checked)
+                Test.TestResult = true;
+            if (rbNo.Checked)
+            {
+                Test.TestResult = false;
+             
+                
+            }
+
+            Test.Notes = txtNotes.Text;
+            Test.CreatedByUserID = ClsGloabalSettings.CurrentUser.UserID;
+            Test.TestAppointmentID = TestAppointmentID;
+            TestAppointment.IsLocked = true;
+            if (Test.TakeTest())
+            {
+                MessageBox.Show("Data Saved Successfuly ");
+                TestAppointment.IsLocked = true;
+                TestAppointment.Save();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Data Was Not Saved Successfuly ");
+            }
+
+        }
+    }
+}
